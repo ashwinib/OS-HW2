a15acbc85d6015d97adc76597c8d976744e4aaa5
ashwinib <ashwini.p.bhat@gmail.com>
2011-02-25 00:48:22 -0800

    Working p3anil.c without some prints

--- p3/p3.c
+++ p3/p3.c
@@ -68,11 +68,6 @@ unsigned int s;
    		     	}
 
    		 }
-	if(th_arg->thread_id == 0)
-   		 {
-   		    		 printf("Final Data = %d",sharedData[0]);
-   		 }
-	//pthread_barrier_wait(&main_barr);
 }
 
 void* string_match(void* arg)
@@ -108,7 +103,7 @@ int main(int argc, char** argv)
 
 		pthread_barrier_init(&level_barr,NULL,thread_num);
 		//pthread_barrier_init(&main_barr,NULL,(thread_num+1));
-		myprintf("\nCreating Threads %d",thread_num);
+		//myprintf("\nCreating Threads %d",thread_num);
 		for ( i=0 ; i<thread_num ; i++ ) {
 			th_arg[i].buf = buf;
 			th_arg[i].str = argv[1];
@@ -122,7 +117,7 @@ int main(int argc, char** argv)
 
 			pthread_create(&(th[i]), NULL, string_match, &(th_arg[i]));
 		}
-		myprintf("\njoining",0);
+		//myprintf("\njoining",0);
 		for ( sum=0, i=0 ; i<thread_num ; i++ ) {
 			pthread_join(th[i], NULL);
 			if ( i>0 && th_arg[i].count > 0 && th_arg[i-1].last >= th_arg[i].first ) {
@@ -133,7 +128,7 @@ int main(int argc, char** argv)
 			}
 			//sum += th_arg[i].count;
 		}
-		myprintf("Main going in wait%d",thread_num+1);
+		//myprintf("Main going in wait%d",thread_num+1);
 		//pthread_barrier_wait(&main_barr);
 		sum= sharedData[0];
 		gettimeofday(&q, NULL);
